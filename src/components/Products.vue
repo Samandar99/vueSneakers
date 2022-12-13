563f1b565d3aad3a1387c504d1bd97f562c968a1
Tim <tim@mail.ru>
2022-12-13 17:32:35 +0500

    added new function click card heart

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,9 +4,20 @@
 
     <div class="sneakers">
       <div class="box" v-for="product in products" :key="product.id">
-        <button class="heart-icon">
+        <button class="heart-icon" @click="favorite(product.id)">
           <img src="@/assets/images/heart.svg" alt="" />
         </button>
+
+        <div
+          v-for="(card, index) in cards"
+          :key="index"
+          @click="isfavorite(card)"
+        >
+          <button class="heart-icon" v-if="card === product.id">
+            <img src="@/assets/images/hearlaev.svg" alt="" />
+          </button>
+        </div>
+
         <img class="nike-sneaker" :src="product.images" alt="" />
         <h5>{{ product.name }}</h5>
         <div class="boxButton">
@@ -14,20 +25,21 @@
             <span>Price:</span>
             <b>{{ product.price }}</b>
           </div>
+
+          <button class="save-icon" v-if="buttonClick === product.id">
+            <img class="icon-save" src="@/assets/images/saveicon.svg" alt="" />
+          </button>
+
           <button
             class="button-iconPlus"
             @click="send(true, product.id)"
-            v-if="!buttonClick"
+            v-else
           >
             <img
               class="icon_plus"
               src="@/assets/images/VectorPlus.svg"
               alt="Plus"
             />
-          </button>
-
-          <button class="save-icon" v-if="buttonClick === product.id ">
-            <img class="icon-save" src="@/assets/images/saveicon.svg" alt="" />
           </button>
         </div>
       </div>
@@ -45,17 +57,36 @@ export default {
 
   data() {
     return {
-      buttonClick: "",
+      buttonClick: true,
+      heart: true,
+      cards: [],
+      // card: this.products[0],
     };
   },
+
   methods: {
+    favorite(id) {
+      this.cards.push(id);
+      console.log(this.cards);
+    },
+    isfavorite(number) {
+      // console.log(number);
+
+      let dupChars = this.cards.filter((element, index) => {
+        return this.cards.indexOf(element) !== index;
+      });
+      this.cards = dupChars;
+      
+      console.log(dupChars);
+    },
+
     send(name, id) {
       console.log(name);
       this.$emit("send", name);
-      let index = this.products.findIndex((todo) => todo.id === id);
-
+      let index = this.products.findIndex((todo) => {
+        return todo.id === id;
+      });
       this.buttonClick = ++index;
-
       console.log(++index);
     },
   },
